7116c5ccbf1e0cf59f4d1e59d1145c091ad40d16
santtuniskanen <santtuniska02@gmail.com>
2023-11-14 12:06:43 +0200

    wrote a test for get request

--- showtime-squad_backend/src/test/java/com/showtimesquad/showtimesquad/endpoints/integration/unit/controller/ControllerTest.java
+++ showtime-squad_backend/src/test/java/com/showtimesquad/showtimesquad/endpoints/integration/unit/controller/ControllerTest.java
@@ -6,14 +6,25 @@ import com.showtimesquad.showtimesquad.controller.Controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ControllerTest {
+
+    @LocalServerPort
+    private int port;
+
     @Autowired
-    private Controller controller;
+    private TestRestTemplate template;
 
     @Test
-    void contextLoads() throws Exception {
-        assertThat(controller).isNotNull();
+    void shouldReturnOK() {
+        final ResponseEntity<Controller> response = template.getForEntity(String.format("http://localhost:%d/movies"
+                , port), Controller.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
     }
 }
