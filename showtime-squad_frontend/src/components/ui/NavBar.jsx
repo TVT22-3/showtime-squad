c0a8a70158878b10ed3c93d28cf5e669c0590a96
Jylant <t2jyan00@students.oamk.fi>
2023-11-15 12:20:18 +0200

    Basics ready, problems w/ dropdowns remain

--- showtime-squad_frontend/src/components/ui/NavBar.jsx
+++ showtime-squad_frontend/src/components/ui/NavBar.jsx
@@ -1,34 +1,53 @@
+// path: showtime-squad_frontend/src/components/ui/NavBar.jsx
+
 import './NavBar.css'
+import sitemap from '../../data/sitemap.json'
+
+const sitemapArray = Object.values(sitemap)
+console.log(sitemapArray)
+
+function NavElement(sitemapKey) {
+    // Used to create a nav element for the NavBar component
+    // const guest = sitemapKey.guests; (not yet implemented)
+    const subpages = hasKey(sitemapKey, "subpages")
+    console.log("component partly implemented")
+    return (
+        <li className={subpages ? "dropdown" : ""}>
+            <a href={sitemapKey.path}>{sitemapKey.title}</a>
+            
+        </li>
+    )
+}
+
+function hasKey(obj, key) {
+    // Used to check if a key exists in an object
+    console.log("component might not be properly implemented yet")
+    return Object.prototype.hasOwnProperty.call(obj, key)
+}
 
 function NavBar() {
     //TODO: Implement
-    console.log("component not properly implemented")
-
+    console.log("component might not be properly implemented yet")
+    console.log(sitemapArray)
     return (
         <nav className='nav-bar'>
-                <ul className='nav-items'>
-                    <li>Home</li>
-                    <li className='dropdown'>Movies
-                        <ul className='dropdown-content'>
-                            <li>Movie Category</li>
-                            <li>Movie Category</li>
-                            <li>Movie Category</li>
-                            <li>Movie Category</li>
-                            <li>Movie Category</li>
-                        </ul>
-                    </li>
-                    <li className='dropdown'>News
-                        <ul className='dropdown-content'>
-                            <li>News Category</li>
-                            <li>News Category</li>
-                            <li>News Category</li>
-                            <li>News Category</li>
-                            <li>News Category</li>
-                        </ul>
-                    </li>
-                    <li>Profile</li>
-                </ul>
-            </nav>
+            <ul className='nav-items'>
+                {sitemapArray.map((key) => {
+                    NavElement(key)
+                    const subpages = hasKey(key, "subpages");
+                    return (
+                        <>
+                            {NavElement(key)}
+                            {subpages ? (
+                                <ul className="dropdown-content">
+                                    {Object.values(key.subpages).map((subKey) => NavElement(subKey))}
+                                </ul>
+                            ) : null}
+                        </>
+                    );
+                })}
+            </ul>
+        </nav>
 
     )
 }
