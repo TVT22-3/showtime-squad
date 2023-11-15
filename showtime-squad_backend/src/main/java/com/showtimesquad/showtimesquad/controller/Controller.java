4e367efaa29b45a16552b5c26fde400ec8c12a57
santtuniskanen <santtuniska02@gmail.com>
2023-11-15 13:20:27 +0200

    fixed dependencies and added README to resources

--- showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/Controller.java
+++ showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/Controller.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.awt.*;
-
 @RestController
 public class Controller {
     @RequestMapping("/")
