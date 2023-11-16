19252c1f88e91b91712b90735053440bece0e5ff
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-11-16 19:47:10 +0000

    forgot to change test to reflect new boolean

--- showtime-squad_frontend/src/components/ui/__tests__/Footer.test.jsx
+++ showtime-squad_frontend/src/components/ui/__tests__/Footer.test.jsx
@@ -12,26 +12,7 @@ test('render Footer without any props', () => {
 })
 
 test('render Footer with sitemap.json as guest', () => {
-    render(<Footer sitemap={actualSitemap} isGuest={true} />)
-
-    const footerElement = screen.getByRole('contentinfo')
-
-    expect(footerElement).toBeInTheDocument()
-    expect(footerElement).toHaveClass('footer')
-
-    //expect sitemap column to exist
-    const elementWithHomeText = screen.getByText(/sitemap/i)
-    expect(elementWithHomeText).toBeInTheDocument()
-
-    //expect all pages available to guests to be visible
-    recursivelyIterateThroughPages(actualSitemap, true, (title) => {
-        const titleElements = screen.getAllByText(new RegExp(`${title}`, 'i'))
-        expect(titleElements.length).toBeGreaterThan(0)
-    })
-})
-
-test('render Footer with sitemap.json as user', () => {
-    render(<Footer sitemap={actualSitemap} isGuest={false} />)
+    render(<Footer sitemap={actualSitemap} loggedIn={false} />)
 
     const footerElement = screen.getByRole('contentinfo')
 
@@ -49,11 +30,30 @@ test('render Footer with sitemap.json as user', () => {
     })
 })
 
-function recursivelyIterateThroughPages(pages, isGuest, assertions) {
+test('render Footer with sitemap.json as user', () => {
+    render(<Footer sitemap={actualSitemap} loggedIn={true} />)
+
+    const footerElement = screen.getByRole('contentinfo')
+
+    expect(footerElement).toBeInTheDocument()
+    expect(footerElement).toHaveClass('footer')
+
+    //expect sitemap column to exist
+    const elementWithHomeText = screen.getByText(/sitemap/i)
+    expect(elementWithHomeText).toBeInTheDocument()
+
+    //expect all pages available to guests to be visible
+    recursivelyIterateThroughPages(actualSitemap, true, (title) => {
+        const titleElements = screen.getAllByText(new RegExp(`${title}`, 'i'))
+        expect(titleElements.length).toBeGreaterThan(0)
+    })
+})
+
+function recursivelyIterateThroughPages(pages, loggedIn, assertions) {
     Object.entries(pages).forEach(([key, page]) => {
         // guard clause to check if user is logged in
         if (page.show &&
-            (isGuest && page.show === 'user') || (!isGuest && page.show === 'guest')) {
+            (!loggedIn && page.show === 'user') || (loggedIn && page.show === 'guest')) {
             return
         }
 
@@ -61,7 +61,7 @@ function recursivelyIterateThroughPages(pages, isGuest, assertions) {
         if (page.subpages) {
             // recurse into subpages
             return (
-                recursivelyIterateThroughPages(page.subpages, isGuest, assertions)
+                recursivelyIterateThroughPages(page.subpages, loggedIn, assertions)
             )
         } else {
             // recursion stops when no more subpages are met
