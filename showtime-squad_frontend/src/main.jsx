9709e5d63202208469f662dfdf5483d2df37d244
mikakorkiakoski <mika.korkiakoski@icloud.com>
2023-11-17 14:34:33 +0200

    Updated main.jsx hotfix

--- showtime-squad_frontend/src/main.jsx
+++ showtime-squad_frontend/src/main.jsx
@@ -7,6 +7,6 @@ import Home from './pages/Home/Home.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    { <Home />}
+     <Home />
   </React.StrictMode>,
 )
