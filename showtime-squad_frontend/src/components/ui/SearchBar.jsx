a5599ccd4a849172b5849352d44ddcb8f84cbd6e
mikakorkiakoski <mika.korkiakoski@icloud.com>
2023-11-20 12:12:54 +0200

    Nested search bar css

--- showtime-squad_frontend/src/components/ui/SearchBar.jsx
+++ showtime-squad_frontend/src/components/ui/SearchBar.jsx
@@ -1,11 +1,11 @@
-import './SearchBar.css'
+import './SearchBar.scss'
 
 function SearchBar() {
     //TODO: Implement
     console.log("component not properly implemented")
 
     return (
-        <search className='search-bar'>
+        <search id="search-bar" className='search-bar'>
             <form id='search-form' className='search-form' method='GET' action='/movies'>
                 <input id='example-search' name='param' type='text' placeholder='Search...' />
                 <button type='submit'><i className='fa fa-search'></i></button>
