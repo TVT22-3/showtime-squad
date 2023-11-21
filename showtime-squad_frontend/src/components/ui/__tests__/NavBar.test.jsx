c23862e3005029777323f9b2820d353d39af082b
Jylant <t2jyan00@students.oamk.fi>
2023-11-21 11:56:44 +0200

    tests deepened, build test added

--- showtime-squad_frontend/src/components/ui/__tests__/NavBar.test.jsx
+++ showtime-squad_frontend/src/components/ui/__tests__/NavBar.test.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import NavBar, { NavElement, SubElement, hasKey } from '../NavBar';
+import  { NavBar, NavElement, SubElement, hasKey, build } from '../NavBar';
 
 describe('NavBar', () => {
     test('renders NavBar component', () => {
         render(<NavBar />);
-        // Add your assertions here
+        const navBarElement = screen.getByTestId('navbar');
+        expect(navBarElement).toBeInTheDocument();
     });
 
     test('renders NavElement component', () => {
         render(<NavElement />);
-        // Add your assertions here
+        const navElement = screen.getByTestId('nav-element');
+        expect(navElement).toBeInTheDocument();
     });
 
     test('renders SubElement component', () => {
         render(<SubElement />);
-        // Add your assertions here
+        const subElement = screen.getByTestId('sub-element');
+        expect(subElement).toBeInTheDocument();
     });
 
+    test('hasKey function returns true for existing key', () => {
+        const obj = { key: 'value' };
+        const keyExists = hasKey(obj, 'key');
+        expect(keyExists).toBe(true);
+    });
+
+    test('hasKey function returns false for non-existing key', () => {
+        const obj = { key: 'value' };
+        const keyExists = hasKey(obj, 'nonExistingKey');
+        expect(keyExists).toBe(false);
+    });
+
+    test('build function returns a valid sitemap', () => {
+        const sitemap = build();
+        expect(sitemap).toBeDefined();
+        expect(Array.isArray(sitemap)).toBe(true);
+        expect(sitemap.length).toBeGreaterThan(0);
+    });
 });
