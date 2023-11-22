0256e6d7c3b8d54aeae228367217b27bf34c1013
santtuniskanen <santtuniska02@gmail.com>
2023-11-22 13:18:03 +0200

    added mockito package

--- showtime-squad_backend/src/test/java/com/showtimesquad/showtimesquad/endpoints/integration/unit/controller/AuthTest.java
+++ showtime-squad_backend/src/test/java/com/showtimesquad/showtimesquad/endpoints/integration/unit/controller/AuthTest.java
@@ -2,24 +2,34 @@ package com.showtimesquad.showtimesquad.endpoints.integration.unit.controller;
 
 import com.showtimesquad.showtimesquad.controller.AuthController;
 
-import net.bytebuddy.asm.Advice;
+import com.showtimesquad.showtimesquad.model.request.SignupRequest;
+import com.showtimesquad.showtimesquad.repository.RoleRepository;
+import com.showtimesquad.showtimesquad.repository.UserRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class AuthTest {
+    UserRepository userRepository = mock(UserRepository.class);
+    RoleRepository roleRepository = mock(RoleRepository.class);
 
-    @LocalServerPort
-    private int port;
+    AuthController authController = new AuthController();
 
-    @Autowired
-    private TestRestTemplate template;
 
     @Test
-    void userShouldExist() {
-        ;
+    void creatingUserReturnsOK() throws Exception {
+        SignupRequest signupRequest = new SignupRequest();
+        ResultActions result = MockMvc.perform(post("/signup")
+                .contentType("application.json")
+                .content());
+        result.andExpect(status().isOk());
     }
 }
