023da8c852a4eadc8afc3eaf742ad0847be087b4
santtuniskanen <santtuniska02@gmail.com>
2023-11-27 11:56:10 +0200

    renamed controller to examplecontroller

--- showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/ExampleController.java
+++ showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/ExampleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class Controller {
+public class ExampleController {
     @RequestMapping("/")
     public String welcome() {
         return "Welcome!";
