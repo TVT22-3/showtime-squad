2df5645288f073697a8ab2959a97cbe031334b40
santtuniskanen <santtuniska02@gmail.com>
2023-11-27 13:58:32 +0200

    created Profile page files

--- showtime-squad_frontend/src/main.jsx
+++ showtime-squad_frontend/src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Profile from './components/ui/Profile.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
     {/* <Home /> */}
     {/* <Login /> */}
     {/* <Register /> */}
     {/* <LoginRegisterHandler /> */}
+    <Profile />
   </React.StrictMode>,
 )
