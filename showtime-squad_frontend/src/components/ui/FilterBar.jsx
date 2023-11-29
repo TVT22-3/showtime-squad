019705d6dd6881704415eb2c861a650ce4541efe
mikakorkiakoski <mika.korkiakoski@icloud.com>
2023-11-29 14:09:16 +0200

    implementations

--- showtime-squad_frontend/src/components/ui/FilterBar.jsx
+++ showtime-squad_frontend/src/components/ui/FilterBar.jsx
@@ -1,82 +1,60 @@
 // path: showtime-squad_frontend/src/components/ui/NavBar.jsx
-
+import React from 'react'
 import './FilterBar.scss'
 import sitemap from '../../data/sitemap.json'
-import { useLoginStatus } from '../../context/UserContext';
 
 const sitemapArray = Object.values(sitemap)
-//console.log(sitemapArray)
-//console.log("placeholder login status is set to: " + loginStatus)
 
-function NavElement(sitemapKey, loginStatus) {
-    // Used to create a nav element for the NavBar component
-    const show = sitemapKey.show;
-    const subpages = hasKey(sitemapKey, "subpages")
-    console.log(subpages)
-    console.log("component partly implemented")
+function NavElement(sitemapKey) {
+  const show = sitemapKey.show
+  const subpages = hasKey(sitemapKey, "subpages")
 
-    // guard clause to check if the user is logged in and if the element is for guests only
-    if ((show=="user"&&loginStatus==false)||(show=="guest"&&loginStatus==true)){
-        return null;
-    }
-    return (
-        <li className={subpages ? 'dropdown' : ''}>
-            <a href={sitemapKey.path}>{sitemapKey.title}</a>
-            {subpages ? 
-            <ul className='dropdown-content'>
-                {Object.values(sitemapKey.subpages).map((key) => (
-                    SubElement(key)
-                ))}
-            </ul> : null}
-        </li>
-    )
+  return (
+    <li className={subpages ? 'dropdown' : ''}>
+      <a href={sitemapKey.path}>{sitemapKey.title}</a>
+      {subpages ?
+        <ul className='dropdown-content'>
+          {Object.values(sitemapKey.subpages).map((key) => (
+            SubElement(key)
+          ))}
+        </ul> : null}
+    </li>
+  )
 }
 
-function SubElement(submapKey, loginStatus) {
-    console.log("in subelement")
-    console.log(submapKey)
-    const show = submapKey.show;
-    console.log(show)
-    
-    // guard clause
-    if ((show=="user"&&loginStatus==false)||(show=="guest"&&loginStatus==true)){
-        return null;
-    }
-    return (
-        <li>
-            <a href={submapKey.path}>{submapKey.title}</a>
-        </li>
-    )
+function SubElement(submapKey) {
+  const show = submapKey.show
+
+ 
+
+  return (
+    <li key={submapKey.title}>
+      <a href={submapKey.path}>{submapKey.title}</a>
+    </li>
+  )
 }
 
 function hasKey(obj, key) {
-    // Used to check if a key exists in an object
-    console.log("component might not be properly implemented yet")
-    return Object.prototype.hasOwnProperty.call(obj, key)
+  return Object.prototype.hasOwnProperty.call(obj, key)
 }
 
 function FilterBar() {
+  
+  const filteredSitemapArray = sitemapArray.filter(
+    (key) => key.title.toLowerCase() === 'top rated' || key.title.toLowerCase() === 'popular'
+  )
 
-    const { loginStatus } = useLoginStatus();
-    //TODO: Implement
-    console.log("component might not be fully implemented yet")
-    console.log("Rendering MoviesPage") // Add this line
-    //console.log(sitemapArray)
-    
-    return (
-        <nav id='nav-bar'>
-            <ul className='nav-items'>
-                {sitemapArray.map((key) => {
-                    return (
-                        <>
-                        {NavElement(key, loginStatus)}
-                        </>
-                    );
-                })}
-            </ul>
-        </nav>
-
-    )
+  return (
+    <nav id='nav-bar'>
+      <ul className='nav-items'>
+        {filteredSitemapArray.map((key) => (
+          <React.Fragment key={key.title}>
+            {NavElement(key)}
+          </React.Fragment>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default FilterBar
--- showtime-squad_frontend/src/components/ui/Movies.jsx
+++ showtime-squad_frontend/src/components/ui/Movies.jsx
@@ -1,40 +1,60 @@
-// MovieComponent.jsx
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Movies.scss'; // Import the SCSS file
 
 const Movies = () => {
-  const [movieData, setMovieData] = useState(null);
+  const [movieData, setMovieData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const loaderRef = useRef(null);
 
   useEffect(() => {
     const fetchMovieData = async () => {
+      setLoading(true);
+
       try {
-        const response = await fetch('http://localhost:8080/movies');
+        const response = await fetch(`http://localhost:8080/movies?page=${page}`);
         const data = await response.json();
-        setMovieData(data.results); // Assuming the movies are in a 'results' array
+        setMovieData((prevData) => [...prevData, ...data.results]);
+        setPage((prevPage) => prevPage + 1);
       } catch (error) {
         console.error('Error fetching movie data:', error);
       }
+
+      setLoading(false);
     };
 
-    fetchMovieData();
-  }, []); // Empty dependency array means this effect runs once after the initial render
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        fetchMovieData();
+      }
+    }, { threshold: 1 });
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
+  }, [page]); // Dependency on page to trigger fetch when page changes
 
   return (
     <div className="movie-container">
-      {movieData ? (
-        movieData.map((movie) => (
-          <div key={movie.id} className="movie-card">
-            <p>{movie.title}</p>
-            {/*<p>Overview: {movie.overview}</p>*/}
-            <p>Release Date: {movie.release_date}</p>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            {/* Add more details as needed */}
-          </div>
-        ))
-      ) : (
-        <p>Loading movie data...</p>
-      )}
+      {movieData.map((movie) => (
+        <div key={movie.id} className="movie-card">
+          <p>{movie.title}</p>
+          <p>Release Date: {movie.release_date}</p>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+          {/* Add more details as needed */}
+        </div>
+      ))}
+
+      {loading && <p>Loading more movies...</p>}
+
+      <div ref={loaderRef}></div>
     </div>
   );
 };
