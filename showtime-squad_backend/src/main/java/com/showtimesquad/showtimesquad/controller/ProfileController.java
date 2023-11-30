1ed9506468dc27ff9b8f159033734c8b430ffde3
Santtu Niskanen <91943883+santtuniskanen@users.noreply.github.com>
2023-11-30 12:18:18 +0200

    Update ProfileController.java

--- showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/ProfileController.java
+++ showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/ProfileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping({ "/api/users", "/profile", "/users" })
+@RequestMapping({ "/api/profile", "/profile", "/users" })
 public class ProfileController {
 
     @GetMapping("/{username}")
