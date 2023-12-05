09abd0bd992a4e8b62b650db78eaa82f4fa3b962
santtuniskanen <santtuniska02@gmail.com>
2023-12-05 13:53:48 +0200

    fixed more issues

--- showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/ProfileController.java
+++ showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/controller/ProfileController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping({ "/api/profile", "/profile", "/users" })
+@RequestMapping({ "/api/users", "/profile", "/users" })
 public class ProfileController {
 
     private final UserDetailsServiceImpl userDetailsService;
--- showtime-squad_frontend/src/main.jsx
+++ showtime-squad_frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Profile from './pages/Profile/Profile.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Login /> */}
     {/* <Register /> */}
     {/* <LoginRegisterHandler /> */}
+    {/*<Profile />*/}
   </React.StrictMode>,
 )
