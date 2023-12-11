e1d2e0ddb3a0b2b47006894095e6d7b7496e03f0
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-12-11 11:40:57 +0000

    fix semantic li error

--- showtime-squad_frontend/src/components/containers/NewsBlock.jsx
+++ showtime-squad_frontend/src/components/containers/NewsBlock.jsx
@@ -3,11 +3,11 @@ import { getXML } from '../../utils/GenericHTTPMethods';
 function NewsBlock({ signal }) {
 
     return (
-        <li>
+        <div>
             <h6>{signal.value.title ? signal.value.title : '???'}</h6>
             <p>{signal.value.synopsis ? signal.value.synopsis : '...'}</p>
             <a href={signal.value.url ? signal.value.url : '#'}>Read more...</a>
-        </li>
+        </div>
     )
 }
 
