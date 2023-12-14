3e65f6f6785341b5696660abb7f79503bd815742
Miika Tiihonen <49746961+Wh1teh@users.noreply.github.com>
2023-12-14 18:44:55 +0000

    should work now

--- showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/model/User.java
+++ showtime-squad_backend/src/main/java/com/showtimesquad/showtimesquad/model/User.java
@@ -9,55 +9,81 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "users",
-       uniqueConstraints = {
-           @UniqueConstraint(columnNames = "username"),
-           @UniqueConstraint(columnNames = "email")
-       })
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class User {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @NotBlank
-  @Size(max = 20)
-  private String username;
+    @NotBlank
+    @Size(max = 20)
+    private String username;
 
-  @NotBlank
-  @Size(max = 50)
-  @Email
-  private String email;
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
 
-  @NotBlank
-  @Size(max = 120)
-  private String password;
+    @NotBlank
+    @Size(max = 120)
+    private String password;
 
-  @Column(name = "profile_pic")
-  @NotBlank
-  @Size(max = 120)
-  private String profilePicture;
+    @Column(name = "profile_pic")
+    @NotBlank
+    @Size(max = 120)
+    private String profilePicture;
 
-  @ManyToMany(fetch = FetchType.LAZY)
-  @JoinTable(name = "user_roles", 
-             joinColumns = @JoinColumn(name = "user_id"),
-             inverseJoinColumns = @JoinColumn(name = "role_id"))
-  private Set<Role> roles = new HashSet<>();
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
-  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-  private Set<MovieReviews> movieReviews = new HashSet<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<MovieReviews> movieReviews = new HashSet<>();
 
+    @ManyToMany(mappedBy = "users")
+    private Set<Group> groups = new HashSet<>();
 
-  public User() {
-  }
+    @ManyToMany(mappedBy = "joinRequests")
+    private Set<Group> joinRequests = new HashSet<>();
 
-  public User(String username, String email, String password, String profilePicture) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.profilePicture = profilePicture;
-  }
+    public User() {
+    }
 
-  // getters and setters
+    public User(String username, String email, String password, String profilePicture) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.profilePicture = profilePicture;
+    }
+
+    // getters and setters
+
+    public Set<MovieReviews> getMovieReviews() {
+        return this.movieReviews;
+    }
+
+    public void setMovieReviews(Set<MovieReviews> movieReviews) {
+        this.movieReviews = movieReviews;
+    }
+
+    public Set<Group> getGroups() {
+        return this.groups;
+    }
+
+    public void setGroups(Set<Group> groups) {
+        this.groups = groups;
+    }
+
+    public Set<Group> getJoinRequests() {
+        return this.joinRequests;
+    }
+
+    public void setJoinRequests(Set<Group> joinRequests) {
+        this.joinRequests = joinRequests;
+    }
 
     public Long getId() {
         return this.id;
@@ -100,7 +126,7 @@ public class User {
     }
 
     public String getProfilePicture() {
-      return profilePicture;
+        return profilePicture;
     }
 
     public void setProfilePicture(String profilePicture) {
