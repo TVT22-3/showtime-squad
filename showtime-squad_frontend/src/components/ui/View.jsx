b2b7dbdb7f6cac468c6135499395f32f92ba155e
Jylant <t2jyan00@students.oamk.fi>
2023-12-15 18:30:41 +0200

    stash commit

--- showtime-squad_frontend/src/components/ui/View.jsx
+++ showtime-squad_frontend/src/components/ui/View.jsx
@@ -1,5 +1,4 @@
-import { signal } from '@preact/signals-react'
-import { createSignal, useSignal} from '@preact/signals-react'
+import { Signal, signal, useSignal} from '@preact/signals-react'
 import { getRequest, postRequest, putRequest, deleteRequest} from "../../utils/GenericHTTPMethods"
 
 import './View.css'
@@ -8,25 +7,12 @@ import Adder from '../atoms/Adder.jsx'
 
 import { OptionsButtonContextProvider } from '../../context/OptionsButtonContext.jsx';
 
-// Mock data to hold information on what ViewBlocks to render for the user.
-// Real info will be loaded from db.
-// 'blocks' will contain an array of (block) objects, that all contain its type,
-// using objects since these might be expanded to include size and other info.
-// Root object may be expanded to include other meta info like order.
-const mockBlockInfoContainer = {
-    blocks: [
-        { type: { category: "movies", option: "top movies" } },
-        { type: { category: "text", option: "news" } },
-        { type: { category: "movies", option: "favorites" } }
-    ],
-}
-
 const apiUrl = import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL
 
 
-const blockInfoContainerSignal = createSignal(getRequest(
+const blockInfoContainerSignal = signal(getRequest(
         apiUrl + "/api/lists/user/"+ sessionStorage.getItem(
-                "username"), true));
+                "username")));
 
 function View() {
     //TODO: Implement
@@ -35,15 +21,21 @@ function View() {
     // use signal to get blockInfoContainer from db
     const [blockInfoContainer, send] = useSignal(blockInfoContainerSignal);
 
-
-        //let blockInfoContainer = signal(mockBlockInfoContainer) // TODO: get from db
-
     function handleAdder() {
         console.log("clicked adder!")
-        let modify = blockInfoContainer.blocks;
-        modify.push({ type: { category: "movies", option: "free pick" } })
+        const newListName = prompt("Please enter a name for your new list", "My List");
+        if (newListName === null || newListName === "") { 
+            alert("Please enter a valid name for your new list");
+            return;
+        }
+
+        let modify = blockInfoContainer.value;
+        modify.push({ listname: newListName })
         // update signal
         send({ ...blockInfoContainer, blocks: modify })
+
+        // update db
+        postRequest(apiUrl + "/api/lists/create", { listname: newListName, movieIds: [] })
 
     }
 
